f310f7e1a359290f66495343ef9f05c26f936811
Megospc <97227980+Megospc@users.noreply.github.com>
2023-10-14 10:36:07 +0300

    Add files via upload

--- Mandelbrot/script.js
+++ Mandelbrot/script.js
@@ -4,10 +4,11 @@ const canvas = document.getElementById('canvas');
 const zoomp = document.getElementById('zoom');
 const pausebtn = document.getElementById('pause');
 const pu = document.getElementById('pu');
+const doublec = document.getElementById('double');
+const doublediv = document.getElementById('doublediv');
 
 const url = new URL(location.href);
 
-var interval;
 var draw, frame, forward, backward;
 var pause = true;
 
@@ -15,12 +16,15 @@ var moveX = 0;
 var moveY = 0;
 var zoom = 1;
 
-if (url.searchParams.get("gpu") == "true") {
+const gpu = url.searchParams.get("gpu") == "true";
+const double = url.searchParams.get("double") == "true";
+
+if (gpu) {
   pu.checked = true;
   
   const scrx = 0.7;
   
-  const width = 600;
+  const width = 640;
   const height = width*scrx;
   const area = 3;
   
@@ -33,7 +37,6 @@ if (url.searchParams.get("gpu") == "true") {
   const move = 0.03;
   
   const contrast = 3;
-  const flevel = (1 << 23);
   
   canvas.width = width;
   canvas.height = height;
@@ -205,47 +208,91 @@ void main() {
   });
 } else {
   pu.checked = false;
+  if (double) doublec.checked = true;
+  doublediv.style.display = "inline";
   
   const scrx = 0.7;
   
-  const width = 200;
+  const iscale = 8;
+  const timeout = 50;
+  
+  const width = double ? 320:640;
   const height = width*scrx;
   const areaw = 5;
   const areah = areaw*scrx;
   
   const max = 4;
-  const pow = 2;
   const iterations = 100;
   
-  const zoomx = 1.02;
-  const zoomz = 10;
-  const zoomm = 1e14;
+  const zoomx = 1.5;
+  const zoomz = 3;
+  const zoomm = double ? 1e30:1e14;
   const move = 0.03;
   
   const contrast = 3;
   
-  function grad(x) {
-    return (
-      (dot(x, [
-        [0, 255],
-        [0.5, 0],
-        [1, 0],
-        [contrast, 0]
-      ]) << 0) +
-      (dot(x, [
-        [0, 0],
-        [0.5, 0],
-        [1, 255],
-        [contrast, 32]
-      ]) << 8) +
-      (dot(x, [
-        [0, 128],
-        [0.5, 0],
-        [1, 64],
-        [contrast, 64]
-      ]) << 16) +
-      (255 << 24)
-    );
+  let grad;
+  
+  switch (url.searchParams.get("theme")) {
+    case "rainbow":
+      grad = function(x) {
+        return (
+          (dot(x, [
+            [0/5, 200],
+            [1/5, 200],
+            [2/5, 0],
+            [3/5, 0],
+            [4/5, 0],
+            [5/5, 200],
+            [contrast, 255]
+          ]) << 0) +
+          (dot(x, [
+            [0/5, 0],
+            [1/5, 200],
+            [2/5, 200],
+            [3/5, 200],
+            [4/5, 0],
+            [5/5, 0],
+            [contrast, 255]
+          ]) << 8) +
+          (dot(x, [
+            [0/5, 0],
+            [1/5, 0],
+            [2/5, 0],
+            [3/5, 200],
+            [4/5, 200],
+            [5/5, 200],
+            [contrast, 255]
+          ]) << 16) +
+          (255 << 24)
+        );
+      };
+      break;
+    default:
+      grad = function(x) {
+        return (
+          (dot(x, [
+            [0, 255],
+            [0.5, 0],
+            [1, 0],
+            [contrast, 0]
+          ]) << 0) +
+          (dot(x, [
+            [0, 0],
+            [0.5, 0],
+            [1, 255],
+            [contrast, 32]
+          ]) << 8) +
+          (dot(x, [
+            [0, 128],
+            [0.5, 0],
+            [1, 64],
+            [contrast, 64]
+          ]) << 16) +
+          (255 << 24)
+        );
+      };
+      break;
   }
   
   function dot(x, arr) {
@@ -262,96 +309,178 @@ void main() {
   
   const ctx = canvas.getContext('2d');
   
-  class Complex {
+  const doub = x => new Double(x);
+  
+  if (double) {
+    moveX = doub(0);
+    moveY = doub(0);
+    zoom = doub(1);
+  }
+  
+  const Complex = double ? class {
+    constructor(r, i) {
+      this.r = doub(r);
+      this.i = doub(i);
+    }
+    
+    sq() {
+      const r = doub(this.r);
+      const i = doub(this.i);
+      
+      this.r = r.mul(r).sub(i.mul(i));
+      this.i = r.mul(i).mul(2);
+    }
+    
+    add(n) {
+      this.r = this.r.add(n.r);
+      this.i = this.i.add(n.i);
+    }
+    
+    length() {
+      return this.r.mul(this.r).add(this.i.mul(this.i));
+    }
+    
+    is() {
+      return this.length().gt(max);
+    }
+  }:class {
     constructor(r, i) {
       this.r = r;
       this.i = i;
     }
     
-    pow(n) {
+    sq() {
       const r = this.r;
       const i = this.i;
       
-      this.r = r**n-i**n;
-      this.i = n*r*i;
+      this.r = r**2-i**2;
+      this.i = 2*r*i;
     }
     
     add(n) {
       this.r += n.r;
       this.i += n.i;
     }
-  }
+    
+    length() {
+      return this.r**2+this.i**2;
+    }
+    
+    is() {
+      return this.length() > max;
+    }
+  };
+  
+  let dids = 0;
   
   draw = function() {
+    const did = ++dids;
+    
     const img = new ImageData(width, height);
     const data = new Uint32Array(img.data.buffer);
     
-    for (let x = 0; x < width; x++) for (let y = 0; y < height; y++) {
-      const k = x+y*width;
-      const c = new Complex(
-        (x/width*areaw-areaw/2)/zoom+moveX,
-        (y/height*areah-areah/2)/zoom+moveY
-      );
+    function f(id, ix = 0, iy = 0, iw = iscale, ih = iscale) {
+      if (id != dids) return;
       
-      let a = new Complex(0, 0);
-      
-      let i;
-      
-      for (i = 0; i < iterations; i++) {
-        a.pow(pow);
-        a.add(c);
+      for (let x = 0; x < width; x += iscale) for (let y = 0; y < height; y += iscale) {
+        const cr = double ? doub(x+ix).div(width/areaw).sub(areaw/2).div(zoom).add(moveX):((x+ix)/width*areaw-areaw/2)/zoom+moveX;
+        const ci = double ? doub(y+iy).div(height/areah).sub(areah/2).div(zoom).add(moveY):((y+iy)/height*areah-areah/2)/zoom+moveY;
         
-        if (a.r**2+a.i**2 > max) break;
+        const c = new Complex(cr, ci);
+        
+        let a = new Complex(0, 0);
+        
+        let i;
+        
+        for (i = 0; i < iterations; i++) {
+          a.sq();
+          a.add(c);
+          
+          if (a.is()) break;
+        }
+        
+        const k = x+y*width;
+        const clr = grad(i/iterations*contrast);
+        for (let l = ix; l < ix+iw; l++) for (let m = iy; m < iy+ih; m++) data[k+l+m*width] = clr;
       }
       
-      data[k] = grad(i/iterations*contrast);
+      ctx.putImageData(img, 0, 0);
     }
     
-    ctx.putImageData(img, 0, 0);
-  };
-  
-  frame = function() {
-    if (!pause) zoom *= zoomx;
+    f(did);
     
-    if (zoom > zoomm) zoom = zoomm;
+    let t = 1;
     
-    draw();
+    function d(s) {
+      for (let x = 0; x < iscale; x += s) for (let y = 0; y < iscale; y += s) if (x > 0 || y > 0) setTimeout(() => f(did, x, y, s, s), timeout*(t++));
+      if (s > 1) for (let x = 0; x < iscale; x += s) for (let y = 0; y < iscale; y += s) d(s/2);
+    }
+    
+    d(iscale/2);
     
     zoomp.innerHTML = "Приближение: "+zoomstr(zoom);
   };
   
-  backward = function() {
-    zoom /= zoomx**zoomz;
+  frame = function() {
+    if (double) zoom = zoom.mul(zoomx);
+    else zoom *= zoomx;
     
-    if (zoom < 1) zoom = 1;
+    if ((double && zoom.gt(zoomm)) || (!double && zoom > zoomm)) zoom = double ? doub(zoomm):zoomm;
+    
+    draw();
+  };
+  
+  backward = function() {
+    if (double) zoom = zoom.div(zoomx**zoomz);
+    else zoom /= zoomx**zoomz;
+    
+    if ((double && zoom.lt(1)) || (!double && zoom < 1)) zoom = double ? doub(1):1;
+    
+    draw();
   };
   forward = function() {
-    zoom *= zoomx**zoomz;
+    if (double) zoom = zoom.mul(zoomx**zoomz);
+    else zoom *= zoomx**zoomz;
     
-    if (zoom > zoomm) zoom = zoomm;
+    if ((double && zoom.gt(zoomm)) || (!double && zoom > zoomm)) zoom = double ? doub(zoomm):zoomm;
+    
+    draw();
   };
   
   canvas.addEventListener("click", e => {
     e.preventDefault();
     
     const b = canvas.getBoundingClientRect();
-    moveX += ((e.clientX-b.left)/b.width*2-1)*area/zoom;
-    moveY += ((e.clientY-b.top)/b.height*2-1)*area*scrx/zoom;
+    
+    if (double) {
+      moveX = moveX.add(doub(((e.clientX-b.left)/b.width*2-1)*areaw/2).div(zoom));
+      moveY = moveY.add(doub(((e.clientY-b.top)/b.height*2-1)*areah/2).div(zoom));
+    } else {
+      moveX += ((e.clientX-b.left)/b.width*2-1)*areaw/zoom/2;
+      moveY += ((e.clientY-b.top)/b.height*2-1)*areah/zoom/2;
+    }
+    
+    frame();
   });
+  
+  document.getElementById('pause').style.display = "none";
 }
 
 function zoomstr(x) {
-  const arr = ["x", "k", "m", "b", "t"];
+  if (typeof x == "object") x = x.toNumber();
+  
+  const arr = ["x", "K", "M", "B", "T", "Q", "Qi", "Sx", "Sp", "Oc", "N"];
   const log = Math.floor(Math.log10(x)/3);
   
-  return (x/1000**log).toFixed(1)+arr[log]
+  return (x/1000**log).toFixed(1)+arr[log];
 }
 
 draw();
 
 function reset() {
-  zoom = 1;
+  zoom = double ? new Double(1):1;
   if (pause) toggle();
+  if (!gpu) draw();
 }
 function download() {
   const a = document.createElement('a');
@@ -369,35 +498,8 @@ function toggle() {
 function tpu() {
   location.href = "?gpu="+pu.checked;
 }
+function doublet() {
+  location.href = "?gpu=false&double="+doublec.checked;
+}
 
-document.addEventListener("keydown", e => {
-  switch (e.code) {
-    case "NumpadAdd":
-      forward();
-      break;
-    case "NumpadSubstract":
-      backward();
-      break;
-    case "Digit0":
-      reset();
-      break;
-    case "Space":
-      toggle();
-      break;
-    case "ArrowLeft":
-      moveX -= (areaw-areaw/2-moveX)/zoom;
-      break;
-    case "ArrowRight":
-      moveX += (areaw-areaw/2-moveX)/zoom;
-      break;
-    case "ArrowUp":
-      moveY -= (areah-areah/2-moveY)/zoom;
-      break;
-    case "ArrowDown":
-      moveY += (areah-areah/2-moveY)/zoom;
-      break;
-  }
-});
-
-if (interval) clearInterval(interval);
-interval = setInterval(frame, 30);
+if (gpu) setInterval(frame, gpu ? 30:300);
